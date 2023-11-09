a9d3d67628f85b73a2a5cf8c56e5f2644fef5289
ferdinan-ops <perdinantumanggor95@gmail.com>
2023-11-09 09:19:36 +0700

    feat: create select component

--- src/pages/Layanan.tsx
+++ src/pages/Layanan.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 const Layanan = () => {
-  return <div>Layanan</div>
+  return (
+    <div>
+      <span>Layanan</span>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  )
 }
 
 export default Layanan
