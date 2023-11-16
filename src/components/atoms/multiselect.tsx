7fd81de911d0798ff5c6fbb3e13c2b7d263dfb2d
ferdinan-ops <perdinantumanggor95@gmail.com>
2023-11-16 15:42:44 +0700

    feat: add new modal

--- src/components/atoms/multiselect.tsx
+++ src/components/atoms/multiselect.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable multiline-ternary */
 import * as React from 'react'
 import { cn } from '@/lib/utils'
 import { Check, X, ChevronsUpDown } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '@/components/ui/command'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
-import { Badge } from '@/components/ui/badge'
 
 export interface OptionType {
   label: string
@@ -15,10 +15,12 @@ interface MultiSelectProps {
   options: OptionType[]
   selected: string[]
   onChange: React.Dispatch<React.SetStateAction<string[]>>
+  placeholder?: string
   className?: string
+  width?: string
 }
 
-function MultiSelect({ options, selected, onChange, className, ...props }: MultiSelectProps) {
+function MultiSelect({ options, selected, onChange, className, placeholder, width, ...props }: MultiSelectProps) {
   const [open, setOpen] = React.useState(false)
 
   const handleUnselect = (item: string) => {
@@ -26,77 +28,70 @@ function MultiSelect({ options, selected, onChange, className, ...props }: Multi
   }
 
   return (
-    <Popover open={open} onOpenChange={setOpen} {...props}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className={`w-full justify-between ${selected.length > 1 ? 'h-full' : 'h-10'}`}
-          onClick={() => {
-            setOpen(!open)
-          }}
-        >
-          <div className="flex gap-1 flex-wrap">
-            {selected.map((item) => (
-              <Badge
-                variant="secondary"
-                key={item}
-                className="mr-1 mb-1"
-                onClick={() => {
-                  handleUnselect(item)
-                }}
-              >
-                {item}
-                <button
-                  className="ml-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      handleUnselect(item)
-                    }
+    <React.Fragment>
+      <Popover open={open} onOpenChange={setOpen} {...props}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full justify-between border-[#D4D7E3] bg-[#F7FBFF] p-4 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#8897AD] focus-visible:ring-offset-2 ring-offset-[#F7FBFF] hover:bg-[#f1f7fd] hover:text-zinc-900"
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            <p className="text-[#A1A1A1] font-normal">{placeholder}</p>
+            <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className={cn('p-0', width ?? 'min-w-full')}>
+          <Command className={className}>
+            <CommandInput placeholder="Search ..." />
+            <CommandEmpty>No item found.</CommandEmpty>
+            <CommandGroup className="max-h-64 overflow-auto">
+              {options.map((option) => (
+                <CommandItem
+                  key={option.value}
+                  onSelect={() => {
+                    onChange(
+                      selected?.includes(option.value)
+                        ? selected?.filter((item) => item !== option.value)
+                        : [...selected, option.value]
+                    )
+                    setOpen(true)
                   }}
-                  onMouseDown={(e) => {
-                    e.preventDefault()
-                    e.stopPropagation()
-                  }}
+                >
+                  <Check
+                    className={cn('mr-2 h-4 w-4', selected?.includes(option.value) ? 'opacity-100' : 'opacity-0')}
+                  />
+                  {option.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      <section className="flex flex-col pt-2 divide-y divide-[##E1E1E1]">
+        {selected.length > 0
+          ? selected.map((item, index) => (
+              <div className="flex justify-between items-center px-5 py-2.5 bg-[#F9FAFC]" key={index}>
+                <p className="text-sm font-medium">{options.find((option) => option.value === item)?.label}</p>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="text-sm hover:text-black"
                   onClick={() => {
                     handleUnselect(item)
                   }}
                 >
                   <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
-                </button>
-              </Badge>
-            ))}
-          </div>
-          <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-full p-0">
-        <Command className={className}>
-          <CommandInput placeholder="Search ..." />
-          <CommandEmpty>No item found.</CommandEmpty>
-          <CommandGroup className="max-h-64 overflow-auto">
-            {options.map((option) => (
-              <CommandItem
-                key={option.value}
-                onSelect={() => {
-                  onChange(
-                    selected.includes(option.value)
-                      ? selected.filter((item) => item !== option.value)
-                      : [...selected, option.value]
-                  )
-                  setOpen(true)
-                }}
-              >
-                <Check className={cn('mr-2 h-4 w-4', selected.includes(option.value) ? 'opacity-100' : 'opacity-0')} />
-                {option.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+                </Button>
+              </div>
+            ))
+          : null}
+      </section>
+    </React.Fragment>
   )
 }
 
-export { MultiSelect }
+export default MultiSelect
---
+++ src/components/organisms/Modal.tsx
@@ -0,0 +1,66 @@
+import { HiXMark } from 'react-icons/hi2'
+
+import { useDisableBodyScroll } from '@/hooks'
+import { cn } from '@/lib/utils'
+import { Button } from '../ui/button'
+
+interface ModalProps {
+  isShow: boolean
+  children: React.ReactNode
+}
+
+const Modal = ({ isShow, children }: ModalProps) => {
+  useDisableBodyScroll(isShow)
+
+  return (
+    <section
+      className={cn(
+        'visible fixed inset-0 z-50 flex items-center justify-center p-4 px-[18px] transition-colors md:px-0',
+        isShow ? 'visible bg-gray-900/75' : 'invisible'
+      )}
+    >
+      <article
+        className={cn(
+          'w-full overflow-hidden rounded-lg p-8 bg-white shadow-xl transition-all duration-300 md:max-w-xl flex flex-col gap-6',
+          isShow ? 'opacity-100' : 'opacity-0'
+        )}
+      >
+        {children}
+      </article>
+    </section>
+  )
+}
+
+interface ModalHeaderProps {
+  children: React.ReactNode
+  setIsShow: (value: boolean) => void
+  className?: string
+}
+
+const Header = ({ children, setIsShow, className }: ModalHeaderProps) => {
+  const handleClose = () => {
+    setIsShow(false)
+  }
+
+  return (
+    <div className="flex items-start justify-between">
+      <div className={className}>{children}</div>
+      <Button variant="outline" size="icon" onClick={handleClose}>
+        <HiXMark className="text-lg text-font" />
+      </Button>
+    </div>
+  )
+}
+
+interface ModalFooterProps {
+  children: React.ReactNode
+}
+
+const Footer = ({ children }: ModalFooterProps) => {
+  return <div className="flex gap-3 justify-end">{children}</div>
+}
+
+Modal.Header = Header
+Modal.Footer = Footer
+
+export default Modal
