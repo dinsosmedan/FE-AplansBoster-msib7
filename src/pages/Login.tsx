f9767e5de0faa47384d18dd6eea57fbe7435a24a
baploarya <aryawardhana7831@gmail.com>
2023-11-22 15:21:24 +0700

    perbaikan

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
   return (
     <main className="py-14 px-36 flex flex-row items-start justify-between h-screen">
       <section className="flex-1 h-full items-center">
-        <div className="w-6/12 h-full flex flex-col justify-between">
+        <div className="w-8/12 h-full flex flex-col justify-between">
           <img src={Logo} alt="logo" className="w-full" />
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-4">
