c3259098369d2860a4d3c6df5d71145cd35f0cb7
baploarya <aryawardhana7831@gmail.com>
2023-11-27 15:43:52 +0700

    set token

--- src/store/server/useAuth.ts
+++ src/store/server/useAuth.ts
@@ -2,12 +2,18 @@ import { loginFn } from '@/api/auth.api'
 import { type AxiosError } from 'axios'
 import { useMutation } from 'react-query'
 import { useNavigate } from 'react-router-dom'
+import { useToken } from '../client'
 
 export const useLogin = () => {
   const navigate = useNavigate()
+  const storeToken = useToken((state) => state.storeToken)
+
   return useMutation(loginFn, {
-    onSuccess: () => {
+    onSuccess: (data) => {
       //
+      // console.log(data.data);
+
+      storeToken(data.data.access_token)
       navigate('/')
 
     },
