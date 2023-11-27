2bc3397241a694769e4db9ae6ab253f2260ae539
baploarya <aryawardhana7831@gmail.com>
2023-11-27 15:36:59 +0700

    implementasi login

--- src/store/server/useAuth.ts
+++ src/store/server/useAuth.ts
@@ -1,15 +1,19 @@
 import { loginFn } from '@/api/auth.api'
 import { type AxiosError } from 'axios'
 import { useMutation } from 'react-query'
+import { useNavigate } from 'react-router-dom'
 
 export const useLogin = () => {
+  const navigate = useNavigate()
   return useMutation(loginFn, {
     onSuccess: () => {
       //
+      navigate('/')
+
     },
     onError: (error: AxiosError) => {
       if (error.response?.status === 401) {
-        window.alert('Email or password is incorrect')
+        // window.alert('Email or password is incorrect')
       }
     }
   })
