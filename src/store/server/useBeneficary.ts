a0e575c615452320ad6487e2320c9477796dfb33
ferdinan-ops <perdinantumanggor95@gmail.com>
2023-12-04 10:38:35 +0700

    feat: finished consume api cek riwayat dukcapil

--- src/store/server/useBeneficary.ts
+++ src/store/server/useBeneficary.ts
@@ -1,4 +1,10 @@
-import { showAssistanceHistoryFn, showBeneficaryByNIKFn, type BeneficiaryQuery, getBeneficiaryFn, showBeneficaryByNIKFn } from '@/api/beneficary.api'
+import {
+  showAssistanceHistoryFn,
+  type BeneficiaryQuery,
+  getBeneficiaryFn,
+  showBeneficaryByNIKFn,
+  showIdentityCheckFn
+} from '@/api/beneficary.api'
 import { useQuery } from 'react-query'
 
 export const useGetBeneficaryByNIK = (nik: string, enabled: boolean) => {
@@ -27,6 +33,12 @@ export const useGetBeneficiary = ({ page, q, idKecamatan, idKelurahan, isDtks }:
 
 export const useGetAssistanceHistory = (nik: string, enabled: boolean) => {
   return useQuery(['assistance-history', nik], async () => await showAssistanceHistoryFn(nik), {
+    enabled
+  })
+}
+
+export const useGetIdentityCheck = (nik: string, enabled: boolean) => {
+  return useQuery(['identity-check', nik], async () => await showIdentityCheckFn(nik), {
     enabled
   })
 }
