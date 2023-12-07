1c02205af802d1aa29a58a31d5c12d92eb8fd482
Krisnaaassss <krisnalucianosiburian29@students.unnes.ac.id>
2023-12-07 10:40:00 +0700

    slicing landing page sktm

--- src/components/organisms/landingPage/CardLandingPage.tsx
+++ src/components/organisms/landingPage/CardLandingPage.tsx
@@ -7,10 +7,12 @@ import { Button } from '../../ui/button'
 interface CardLandingPageProps {
   curvePosition?: 'left' | 'right'
   title: string
-  desc: string
+  desc?: string
   btnText: string
   icon: IconType
   href: string
+  className?: string
+  isHadButtonIcon?: boolean
 }
 
 export default function CardLandingPage({
@@ -19,7 +21,9 @@ export default function CardLandingPage({
   desc,
   btnText,
   icon: Icon,
-  href
+  href,
+  className,
+  isHadButtonIcon = true
 }: CardLandingPageProps) {
   const navigate = useNavigate()
 
@@ -30,15 +34,16 @@ export default function CardLandingPage({
         'p-7 flex flex-col gap-[18px] justify-between rounded-xl bg-white bg-no-repeat hover:shadow-2xl shadow-zinc-500/30 transition-shadow cursor-pointer duration-300',
         curvePosition === 'left'
           ? "bg-[url('@/assets/images/line-curve.svg')]"
-          : "bg-[url('@/assets/images/line-curve-right.svg')]"
+          : "bg-[url('@/assets/images/line-curve-right.svg')]",
+        className
       )}
     >
       {Icon && <Icon className="text-[68px] text-primary" />}
-      <h3 className="font-semibold text-2xl">{title}</h3>
-      <p className="font-medium text-[#666666]">{desc}</p>
-      <Button className="gap-3 text-white">
-        <span>{btnText}</span>
-        <HiArrowRightCircle className="text-xl" />
+      <h3 className="font-semibold text-xl">{title}</h3>
+      {desc && <p className="font-medium  text-[#666666]">{desc}</p>}
+      <Button className="gap-3 text-white py-7">
+        <span className="text-xl">{btnText}</span>
+        {isHadButtonIcon && <HiArrowRightCircle className="text-[30px]" />}
       </Button>
     </article>
   )
