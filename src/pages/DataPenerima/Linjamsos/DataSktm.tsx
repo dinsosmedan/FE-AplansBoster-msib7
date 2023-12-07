a30355860bd7c5babb51a37ced4676e66f56dbe9
raihanoza <raihanoza18@gmail.com>
2023-12-07 10:18:25 +0700

    done

--- src/pages/DataPenerima/Linjamsos/DataSktm.tsx
+++ src/pages/DataPenerima/Linjamsos/DataSktm.tsx
@@ -8,30 +8,73 @@ import { Button } from '@/components/ui/button'
 import { HiArrowPath, HiMagnifyingGlass } from 'react-icons/hi2'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import Pagination from './../../../components/atoms/Pagination'
-import * as React from 'react'
-
+import { useCreateParams, useDisableBodyScroll, useGetParams } from '@/hooks'
+import { useNavigate } from 'react-router-dom'
+import { useGetIndigencyCertificateFn, useGetKecamatan, useGetKelurahan } from '@/store/server'
+import { Action, Loading } from '@/components'
+interface FormValues {
+  q: string
+  kelurahan: string
+  kecamatan: string
+  year: string
+  status: string
+}
 const DataSktm = () => {
   useTitle('Data Penerima / Linjamsos / SKTM ')
-
-  interface FormValues {
-    nik: string
-    kelurahan: string
-    kecamatan: string
-    batch: string
-  }
+  const navigate = useNavigate()
+  const createParams = useCreateParams()
+  const { q, kecamatan, kelurahan, page, year } = useGetParams(['q', 'kecamatan', 'kelurahan', 'page', 'year'])
   const forms = useForm<FormValues>({
-    mode: 'onTouched'
+    defaultValues: {
+      q: '',
+      kecamatan: '',
+      kelurahan: '',
+      year: '',
+      status: ''
+    }
   })
-  const [currentPage, setCurrentPage] = React.useState(1)
+  const areaLevel3 = forms.watch('kecamatan')
+  const { data: listKecamatan } = useGetKecamatan()
+  const { data: listKelurahan } = useGetKelurahan(areaLevel3 ?? kecamatan)
+  const {
+    data: indigencys,
+    refetch,
+    isFetching,
+    isLoading
+  } = useGetIndigencyCertificateFn({
+    page: parseInt(page) ?? 1,
+    idKecamatan: kecamatan,
+    idKelurahan: kelurahan,
+    year,
+    status,
+    q
+  })
+  useDisableBodyScroll(isFetching)
+  const updateParam = (key: any, value: any) => {
+    if (value !== '') {
+      createParams({ key, value })
+      createParams({ key: 'page', value: '' })
+    } else {
+      createParams({ key, value: '' })
+    }
+  }
 
   const onSubmit = async (values: FormValues) => {
-    console.log(values)
-  }
+    updateParam('q', values.q)
+    updateParam('kecamatan', values.kecamatan)
+    updateParam('kelurahan', values.kelurahan)
+    updateParam('year', values.year)
+    updateParam('status', values.status)
 
-  // const handleReset = () => {
-  //   navigate('/data-penerima/dayasos/data-djp')
-  //   forms.reset()
-  // }
+    await refetch()
+  }
+  const handleReset = () => {
+    navigate('/data-penerima/linjamsos/data-sktm')
+    forms.reset()
+  }
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <Container>
@@ -40,18 +83,18 @@ const DataSktm = () => {
         <form onSubmit={forms.handleSubmit(onSubmit)} className="flex flex-col gap-6">
           <div className="grid grid-cols-3 gap-x-5 gap-y-5 mt-6 ">
             <FormField
-              name="nik"
+              name="q"
               control={forms.control}
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input {...field} type="number" placeholder="Masukkan Nama / NIK" />
+                    <Input {...field} type="text" placeholder="Masukkan Nama / NIK" />
                   </FormControl>
                 </FormItem>
               )}
             />
             <FormField
-              name="nik"
+              name="year"
               control={forms.control}
               render={({ field }) => (
                 <FormItem>
@@ -62,21 +105,27 @@ const DataSktm = () => {
               )}
             />
             <FormField
-              name="batch"
+              name="status"
               control={forms.control}
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue placeholder="Pilih Status DTKS" />
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        <SelectItem value="m@example.com">Krisna Asu</SelectItem>
-                        <SelectItem value="m@google.com">Krisna Cuki</SelectItem>
-                        <SelectItem value="m@support.com">The Little Krishna</SelectItem>
+                          <SelectItem value='dtks'>
+                            DTKS
+                          </SelectItem>
+                          <SelectItem value='non-dtks'>
+                            Non DTKS
+                          </SelectItem>
+                          <SelectItem value='prelist'>
+                            PRELIST
+                          </SelectItem>
                       </SelectContent>
                     </Select>
                   </FormControl>
@@ -86,18 +135,7 @@ const DataSktm = () => {
           </div>
           <div className="grid grid-cols-3 gap-x-5 gap-y-5 ">
             <FormField
-              name="nik"
-              control={forms.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input {...field} type="number" placeholder="Masukan Jenis Pengajuan" />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="kelurahan"
+              name="kecamatan"
               control={forms.control}
               render={({ field }) => (
                 <FormItem>
@@ -109,9 +147,11 @@ const DataSktm = () => {
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        <SelectItem value="m@example.com">Krisna Asu</SelectItem>
-                        <SelectItem value="m@google.com">Krisna Cuki</SelectItem>
-                        <SelectItem value="m@support.com">The Little Krishna</SelectItem>
+                      {listKecamatan?.map((item, index) => (
+                          <SelectItem key={index} value={item.id}>
+                            {item.name}
+                          </SelectItem>
+                        ))}
                       </SelectContent>
                     </Select>
                   </FormControl>
@@ -119,21 +159,23 @@ const DataSktm = () => {
               )}
             />
             <FormField
-              name="kecamatan"
+              name="kelurahan"
               control={forms.control}
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} defaultValue={field.value} disabled={areaLevel3 === '' && kecamatan === ''}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue placeholder="Pilih Kelurahan" />
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        <SelectItem value="m@example.com">Krisna Asu</SelectItem>
-                        <SelectItem value="m@google.com">Krisna Cuki</SelectItem>
-                        <SelectItem value="m@support.com">The Little Krishna</SelectItem>
+                      {listKelurahan?.map((item, index) => (
+                          <SelectItem key={index} value={item.id}>
+                            {item.name}
+                          </SelectItem>
+                        ))}
                       </SelectContent>
                     </Select>
                   </FormControl>
@@ -151,16 +193,16 @@ const DataSktm = () => {
                 <SelectItem value=".csv">.csv</SelectItem>
               </SelectContent>
             </Select>
-            <div className="flex items-center gap-3">
-              <Button type="button" variant="outline" className="gap-3 text-primary rounded-lg">
+            <div className='flex gap-3'>
+              <Button type="button" variant="outline" className="gap-3 text-primary rounded-lg" onClick={handleReset}>
                 <HiArrowPath className="text-lg" />
                 <span>Reset</span>
               </Button>
-              <Button className="gap-2 border-none rounded-lg" type="submit">
-                <HiMagnifyingGlass className="text-lg" />
-                <span>Cari Data</span>
+              <Button>
+                <HiMagnifyingGlass className="w-4 h-4 py" />
+                <p className="font-bold text-sm text-white ml-3 w-max">Cari Data</p>
               </Button>
-            </div>
+              </div>
           </section>
         </form>
       </Form>
@@ -168,41 +210,56 @@ const DataSktm = () => {
         <Table>
           <TableHeader className="bg-[#FFFFFF]">
             <TableRow>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Kode Kegiatan</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Nama Kelompok Masyarakat</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">No. </TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Nama Pemohon</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Nama Bersangkutan</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Tahun Pembuatan</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Tanggal Terbit</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Status DTKS</TableHead>
               <TableHead className="text-[#534D59] font-bold text-[15px]">Kecamatan</TableHead>
               <TableHead className="text-[#534D59] font-bold text-[15px]">Kelurahan</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Kelurahan</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Jenis Kegiatan</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Jenis Bantuan</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Jumlah Bantuan Disetujui</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Tahun Pencairan</TableHead>
-              <TableHead className="text-[#534D59] font-bold text-[15px]">Status Pencairan</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Jenis Pengajuan</TableHead>
+              <TableHead className="text-[#534D59] font-bold text-[15px]">Action</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="text-left  bg-[#F9FAFC]">1271092350010008</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">Oza Kristen</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">Perempuan</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">1945</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">Medan</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">Medan</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">Santunan</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">10.000.000</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">10.000.000</TableCell>
-              <TableCell className="text-left  bg-[#F9FAFC]">10.000.000</TableCell>
-            </TableRow>
+          {indigencys?.data?.length !== 0 ? (
+              indigencys?.data.map((item, index) => (
+                <TableRow key={item.id}>
+                  <TableCell className="text-left bg-[#F9FAFC]">
+                    {(indigencys.meta.currentPage - 1) * indigencys.meta.perPage + index + 1}
+                  </TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.applicant?.name ?? '-'}</TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.peopleConcerned?.name ?? '-'}</TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.issueYear ?? '-'}</TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.issueDate ?? '-'}</TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.isDtks ? 'DTKS' : 'Non DTKS'}</TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.applicant?.address.areaLevel3?.name ?? '-'}</TableCell>
+                  <TableCell className="text-center bg-[#F9FAFC]">{item.applicant?.address.areaLevel4?.name ?? '-'}</TableCell>
+                  <TableCell className="flex items-center justify-center bg-[#F9FAFC]">
+                  <Action onDelete={() => console.log('delete')} onDetail={() => console.log('edit')} onEdit={() => console.log('detail')}/>
+        </TableCell>
+        </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={9} className="text-center">
+                    Tidak ada data
+                  </TableCell>
+                </TableRow>
+              )}
           </TableBody>
         </Table>
       </section>
-      <Pagination
-        className="px-5 py-5 flex justify-end"
-        currentPage={currentPage}
-        totalCount={100}
-        pageSize={10}
-        onPageChange={(page) => setCurrentPage(page)}
-      />
+      {(indigencys?.meta?.total as number) > 10 ? (
+          <Pagination
+            className="px-5 py-5 flex justify-end"
+            currentPage={page !== '' ? parseInt(page) : 1}
+            totalCount={indigencys?.meta.total as number}
+            pageSize={30}
+            onPageChange={(page) => createParams({ key: 'page', value: page.toString() })}
+          />
+        ) : null}
     </Container>
   )
 }
