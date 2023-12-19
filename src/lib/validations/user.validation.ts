04d14ccadc143c9d894b6b289b39890e2203f350
ALBERT LUKAS TALUPAN PANGARIBUAN <albertlukastalupan1@gmail.com>
2023-12-19 12:27:50 +0700

    update admin

--- src/lib/validations/user.validation.ts
+++ src/lib/validations/user.validation.ts
@@ -2,16 +2,14 @@ import * as Yup from 'yup'
 
 export const userValidation = Yup.object({
   employeeIdentityNumber: Yup.string()
-    .matches(/^[0-9]+$/, 'NIP harus berupa angka')
-    .required('NIP wajib diisi'),
-  email: Yup.string().email('Format email tidak valid').required('Email wajib diisi'),
-  name: Yup.string().required('Nama wajib diisi'),
+    .matches(/^[0-9]+$/, 'NIP harus berupa angka'),
+  email: Yup.string().email('Format email tidak valid'),
+  name: Yup.string(),
   phoneNumber: Yup.string()
-    .matches(/^[0-9]+$/, 'No. HP harus berupa angka')
-    .required('No. HP wajib diisi'),
-  password: Yup.string().required('Password wajib diisi'),
-  role: Yup.string().required('Role wajib diisi'),
-  isActive: Yup.string().required('Status wajib diisi')
+    .matches(/^[0-9]+$/, 'No. HP harus berupa angka'),
+  password: Yup.string(),
+  role: Yup.string(),
+  isActive: Yup.string()
 })
 
 export type userFields = Yup.InferType<typeof userValidation>
