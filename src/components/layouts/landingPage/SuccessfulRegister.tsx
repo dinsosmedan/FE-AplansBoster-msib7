767f754501f59a55ba9ed67d4c4f298c2e1526c9
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-12-23 16:08:47 +0700

    fix: responsive auth verif

--- src/components/layouts/landingPage/SuccessfulRegister.tsx
+++ src/components/layouts/landingPage/SuccessfulRegister.tsx
@@ -2,13 +2,13 @@ import { Bgsuccessful } from '@/assets'
 
 export default function SuccessfulRegister() {
   return (
-    <div className="w-full flex items-center justify-center pt-[51px] pb-10">
-      <div className="w-[686px] h-[540px] bg-white pt-14 shadow-xl">
-        <div className="py- flex flex-col items-center justify-center gap-8">
+    <div className="w-full flex items-center justify-center pt-[51px]  pb-10">
+      <div className="md:w-[686px] w-[90%] md:h-[560px] bg-white pt-14 shadow-lg rounded-xl">
+        <div className="py- flex flex-col items-center justify-center text-center px-10 pb-20 md:p-0">
           <img src={Bgsuccessful} alt="" />
-          <p className="text-[32px] font-semibold">Selamat, anda telah membuat akun</p>
-          <p className="text-[#8B8B8B] text-xl">Akun Anda Belum Terverifikasi</p>
-          <p className="text-[#8B8B8B] text-xl">Silahkan cek email anda untuk melakukan verifikasi</p>
+          <p className="text-xl font-semibold pt-10 pb-5">Selamat, anda telah membuat akun</p>
+          <p className="text-[#8B8B8B] md:text-md text-sm">Akun Anda Belum Terverifikasi</p>
+          <p className="text-[#8B8B8B] md:text-md text-sm">Silahkan cek email anda untuk melakukan verifikasi</p>
         </div>
       </div>
     </div>
--- src/components/layouts/landingPage/VerifiedAccount.tsx
+++ src/components/layouts/landingPage/VerifiedAccount.tsx
@@ -5,14 +5,14 @@ import { useNavigate } from 'react-router-dom'
 export default function VerifiedAccount() {
   const navigate = useNavigate()
   return (
-    <div className="flex py-14 flex-col justify-center items-center gap-[30px] bg-[#F9F9F9]">
-      <div className="bg-white w-[686px] h-[535px] shadow-xl">
-        <div className="flex flex-col items-center justify-center py-14 gap-6">
+    <div className="flex py-14 flex-col justify-center items-center w-[90%] gap-[30px] bg-[#F9F9F9] ">
+      <div className="bg-white md:w-[686px] w-[100%] h-[480px] shadow-lg rounded-xl">
+        <div className="flex flex-col items-center justify-center py-14">
           <img className="w-[168px] h-[168px]" src={BgVerified} alt="" />
-          <p className="text-[34px] font-semibold">Akun Terverifikasi</p>
-          <p className="text-xl">Selamat, Akun anda berhasil diverifikasi</p>
-          <Button className="w-[200px] h-[60px]" onClick={() => navigate('/user/login')}>
-            <p className="text-white text-[26px] font-semibold">Masuk</p>
+          <p className="text-xl font-semibold pt-10">Akun Terverifikasi</p>
+          <p className="md:text-md text-sm pb-10">Selamat, Akun anda berhasil diverifikasi</p>
+          <Button className="md:w-[160px] w-[150px] md:h-[50px]" onClick={() => navigate('/user/login')}>
+            <p className="text-white text-xl font-semibold">Masuk</p>
           </Button>
         </div>
       </div>
